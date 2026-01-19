68a0bf775d8a8e30f1cf24785887af357f6c8304
Mihail Dimitrov <mihail.dimitrow.2008@gmail.com>
2026-01-19 11:15:05 +0200

    mod: added led lamp

--- arduino_code/code/code.ino
+++ arduino_code/code/code.ino
@@ -1,9 +1,24 @@
+int ledRed = 2;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(ledRed, OUTPUT);
+  digitalWrite(ledRed, HIGH);
 }
 
 void loop() {
   int lightValue = analogRead(A0);
   Serial.println(lightValue);
+
+  if (lightValue < 200)
+  {
+    digitalWrite(ledRed, HIGH);
+  }
+
+  else
+  {
+    digitalWrite(ledRed, LOW);
+  }
+
   delay(1000);
 }
